8d393db7e255a4df3b5fcebc62a3d00d8a5a743e
zohaib <zohaib.99tech@gmail.com>
2023-02-16 00:27:35 +0500

    test

---
+++ gitaction_ts.py
@@ -0,0 +1,15 @@
+import json
+import datetime
+
+def notify_gitaction():
+    dt = datetime.datetime.now()
+    ts = dt.strftime("%Y_%b_%d-%H:%M")
+    data = {
+        'event': 'GIT PUSH',
+        'ts': ts
+    }
+    with open("notify_gitaction.json", 'w', encoding='utf-8') as fd:
+        fd.write(json.dumps(data, indent=4))
+
+if __name__ == "__main__":
+    notify_gitaction()
